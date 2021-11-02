8d48a872a0342659d0ed98a146827a44fbc0b0b7
麋鹿 <1912655018@qq.com>
2021-11-02 17:53:50 +0800

    实验

--- lazy-ui/src/views/web/vm/BDS.vue
+++ lazy-ui/src/views/web/vm/BDS.vue
@@ -8,10 +8,20 @@
     <el-card style="margin-top: 10px">
       <span >用户位置:{{-2148744.3969,4426641.2099,4044655.8564}}</span>
     </el-card>
+    <el-card style="margin-top: 10px">
+      <span >频点参数 B1:{{1561.098+'Hz'}}</span>
+    </el-card>
     <el-card class="card" style="margin-top: 10px">
       <div style="text-align: center">
         <div style="padding-top: 25px;">
           <el-form ref="form" size="medium" label-width="100px">
+            <el-row :gutter="24">
+              <el-col :span="12">
+                <el-form-item prop="time">
+                  <el-input  :style="{width: '100%'}" placeholder="计算时刻如UTC2015-10-01"></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
             <el-row :gutter="24">
               <el-col :span="12">
                 <el-form-item prop="time">
@@ -21,6 +31,13 @@
               <el-col :span="12">
                 <el-form-item prop="time">
                   <el-input  :style="{width: '100%'}" placeholder="计算方位角"></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row :gutter="24">
+              <el-col :span="24">
+                <el-form-item prop="time">
+                  <el-input :style="{width: '100%'}" placeholder="地心张角"></el-input>
                 </el-form-item>
               </el-col>
             </el-row>
@@ -39,21 +56,35 @@
             <el-row :gutter="24">
               <el-col :span="24">
                 <el-form-item  prop="time">
-                  <el-input :style="{width: '100%'}" placeholder="A2"></el-input>
+                  <el-input :style="{width: '100%'}" placeholder="计算刺穿点的日固地磁经纬度"></el-input>
                 </el-form-item>
               </el-col>
             </el-row>
             <el-row :gutter="24">
               <el-col :span="24">
                 <el-form-item  prop="time">
-                  <el-input :style="{width: '100%'}" placeholder="A4"></el-input>
+                  <el-input :style="{width: '100%'}" placeholder="Ai(i=1~9)"></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row :gutter="24">
+              <el-col :span="24">
+                <el-form-item  prop="time">
+                  <el-input :style="{width: '100%'}" placeholder="AO"></el-input>
+                </el-form-item>
+              </el-col>
+            </el-row>
+            <el-row :gutter="24">
+              <el-col :span="24">
+                <el-form-item  prop="time">
+                  <el-input :style="{width: '100%'}" placeholder="计算VTEC"></el-input>
                 </el-form-item>
               </el-col>
             </el-row>
             <el-row :gutter="24">
               <el-col :span="24">
                 <el-form-item prop="time">
-                  <el-input :style="{width: '100%'}" placeholder="垂直电离层延迟改正值"></el-input>
+                  <el-input :style="{width: '100%'}" placeholder="计算投影函数"></el-input>
                 </el-form-item>
               </el-col>
             </el-row>
