21af0c996795e525ccc6ee3c946df8c31f3ba19d
麋鹿 <1912655018@qq.com>
2022-03-04 16:56:29 +0800

    new

---
+++ lazy-admin/src/main/java/com/lazy/Test.java
@@ -0,0 +1,12 @@
+package com.lazy;
+
+/**
+ * @author Lazy
+ * @date 2022/2/8 13:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        Integer integer = 1;
+        int i = integer;
+    }
+}
