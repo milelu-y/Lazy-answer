24370523a3c2cdccc8698ec2ffc51162c3d97c64
麋鹿 <1912655018@qq.com>
2022-03-04 16:40:12 +0800

    new

--- lazy-ui/src/views/web/vm/GSM.vue
+++ lazy-ui/src/views/web/vm/GSM.vue
@@ -3,7 +3,7 @@
     <div><h2>{{exData.title}}</h2></div>
     <el-divider/>
     <div>
-      <el-card style="position: absolute;left: 5px; top: 42px; width: 40%;height:700px;overflow:auto;">
+      <el-card style="position:absolute;left:5px;top:0; width:40%;height:100%;overflow-y:auto;overflow-x:auto;">
         <div slot="header" class="clearfix">
           <span>实验步骤</span>
           <span style="float: right;color: #498c5f" v-if="exData.resource==null">暂未上传实验大纲</span>
@@ -15,40 +15,115 @@
         </div>
       </el-card>
     </div>
-    <el-card>
-      <span>伪距:{{ data.input }}、TGD:{{ data.input2 }}</span>
-    </el-card>
-    <el-card class="card" style="margin-top: 10px">
-      <p v-if="isType">ps:计算各频点的伪距修正值，得到修正后的伪距</p>
-      <p v-else>ps:计算各频点的电离层延迟值</p>
-      <div style="text-align: center">
-        <div style="padding-top: 25px;">
-          <el-form ref="form" size="medium" label-width="100px">
-            <el-row :gutter="24">
-              <el-col :span="24">
-                <el-form-item label="伪距" prop="time">
-                  <el-input :disabled="!isType" :style="{width: '100%'}" v-model="wj" placeholder="伪距"></el-input>
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row :gutter="24" v-if="!isType">
-              <el-col :span="24">
-                <el-form-item label="电离层延迟" prop="time">
-                  <el-input :style="{width: '100%'}" v-model="dl" placeholder="电离层延迟"></el-input>
-                </el-form-item>
-              </el-col>
-            </el-row>
-            <el-row>
-              <el-col :span="24" :style="{ textAlign: 'center' }">
-                <el-button type="primary" @click="handle">
-                  提交
-                </el-button>
-              </el-col>
-            </el-row>
-          </el-form>
+    <div class="clock">
+      <el-button @click="subitfront" size="small">修正前</el-button>
+      <el-button @click="subitafter" size="small">修正后</el-button>
+    </div>
+    <div class="allbody" v-show= "frontlist">
+      <el-card>
+        <div class="user" style="margin-top: 20px;">
+          伪距：
+          <el-input v-model="data.input " style="width: 220px"/>
         </div>
-      </div>
-    </el-card>
+        <div class="frequency" style="margin-top: 20px;">
+          TGD：
+          <el-input v-model="data.input2 " style="width: 220px"/>
+          <el-button @click="refresh" class="properties">刷新</el-button>
+        </div>
+        <!-- <span>伪距:{{ data.input }}、TGD:{{ data.input2 }}</span> -->
+      </el-card>
+
+      <el-card class="card" style="margin-top: 10px">
+        <p style="font-size: 14px;font-weight: 600;">计算各频点修正后的伪距,及各频点的电离层延迟值</p>
+        <!-- <p v-else>计算各频点的电离层延迟值</p> -->
+        <div>
+          <div style="padding-top: 25px;">
+            <el-form ref="form" size="medium" label-width="100px">
+              <el-row :gutter="24">
+                <el-col :span="24">
+                  <el-form-item label="修正后的伪距" prop="time">
+                    <el-input  :style="{width: '50%'}"  v-model="wj" :disabled="timesbj" placeholder="请输入修正后的伪距"></el-input>
+                    <el-button type="primary" @click="handle" class="properties" v-if="operation">
+                      转换
+                    </el-button>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+              <el-row :gutter="24" >
+                <el-col :span="24">
+                  <el-form-item label="电离层延迟" prop="time">
+                    <el-input :style="{width: '50%'}" v-model="dl" :disabled="timesbj1" placeholder="请输入电离层延迟"></el-input>
+                    <el-button type="primary" @click="handle1" class="properties" v-if="operation1">
+                      转换
+                    </el-button>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+              <!-- <el-row>
+                <el-col :span="24" :style="{ textAlign: 'center' }">
+                  <el-button type="primary" @click="handle">
+                    提交
+                  </el-button>
+                </el-col>
+              </el-row> -->
+            </el-form>
+          </div>
+        </div>
+      </el-card>
+    </div>
+    
+    <div class="allbody" v-show= "afterlist">
+      <el-card>
+        <div class="user" style="margin-top: 20px;">
+          修正伪距值：
+          <el-input v-model="data.output.result1" style="width: 220px"/>
+        </div>
+        <div class="frequency" style="margin-top: 20px;">
+          电离层延迟：
+          <el-input v-model="data.input3" style="width: 220px"/>
+          <el-button @click="refresh" class="properties">刷新</el-button>
+        </div>
+        <!-- <span>伪距:{{ data.input }}、TGD:{{ data.input2 }}</span> -->
+      </el-card>
+
+      <el-card class="card" style="margin-top: 10px">
+        <p style="font-size: 14px;font-weight: 600;">计算各频点修正后的伪距,及各频点的电离层延迟值</p>
+        <!-- <p v-else>计算各频点的电离层延迟值</p> -->
+        <div>
+          <div style="padding-top: 25px;">
+            <el-form ref="form" size="medium" label-width="100px">
+              <el-row :gutter="24">
+                <el-col :span="24">
+                  <el-form-item label="伪距" prop="time">
+                    <el-input  :style="{width: '50%'}"  v-model="afternum" :disabled="timesbj" placeholder="请输入伪距"></el-input>
+                    <el-button type="primary" @click="submitafter"  class="properties" v-if="operation">
+                     转换
+                    </el-button>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+              <el-row :gutter="24" >
+                <el-col :span="24">
+                  <el-form-item label="TGD" prop="time">
+                    <el-input :style="{width: '50%'}" v-model="aftername" :disabled="timesbj1" placeholder="请输入TGD"></el-input>
+                    <el-button type="primary" @click="submitafter1" class="properties" v-if="operation1">
+                      转换
+                    </el-button>
+                  </el-form-item>
+                </el-col>
+              </el-row>
+              <!-- <el-row>
+                <el-col :span="24" :style="{ textAlign: 'center' }">
+                  <el-button type="primary" @click="handle">
+                    提交
+                  </el-button>
+                </el-col>
+              </el-row> -->
+            </el-form>
+          </div>
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -61,10 +136,20 @@ export default {
   data() {
     return {
       exData:{},
-      isType: true,
+      // getType:false,
+      // isType: true,
       data: {},
       wj: null,
       dl: null,
+      frontlist:true,
+      afterlist:false,
+      afternum:null,
+      aftername:null,
+      timesbj:false,
+      timesbj1:true,
+      operation:true,
+      operation1:false
+
     }
   },
   created() {
@@ -78,39 +163,149 @@ export default {
     })
   },
   methods: {
+    //刷新按钮
+    refresh(){
+      exp10_shuangpinDianLiCeng().then(response => {
+        console.log(response)
+        this.data = response.data
+      })
+      this.afternum = null
+      this.aftername = null
+      this.wj = null
+      this.dl = null
+      this.timesbj = false
+      this.timesbj1 = true
+      this.operation = true
+      this.operation1 = false
+    },
     handle() {
-      if (this.isType) {
-        var v = this.wj
-        if ((v * 1) === this.data.output.result1) {
-          this.$confirm('转换正确!', '提示', {
-            confirmButtonText: '下一题',
-            type: 'success'
-          }).then(() => {
-          })
-          this.isType = false
-        } else {
-          this.notifyError("转换错误，请重试")
-        }
+      var v = this.data.output.result1
+      if ((this.wj * 1) === this.data.output.result1) {
+        this.$confirm('转换正确!', '提示', {
+          confirmButtonText: '确定',
+          type: 'success'
+        }).then(() => {
+          this.timesbj = true
+          this.timesbj1 = false
+          this.operation = false
+          this.operation1 = true
+        })
+        this.getType = true
+        this.isType = false
       } else {
-        var v = this.dl
-        if ((v * 1) === this.data.input3) {
+        this.$confirm('转换错误，正确答案为：'+ v, '提示',{
+          confirmButtonText: '确定',
+          type:'warning'
+        }).then(()=>{
+          this.wj = this.data.output.result1
+        })
+      }
+    },
+      handle1(){
+        var v = this.data.input3
+        if ((this.dl * 1) === this.data.input3) {
           this.$confirm('转换正确!', '提示', {
             confirmButtonText: '完成',
             type: 'success'
           }).then(() => {
+            this.timesbj = true
+            this.timesbj1 = true
+            this.operation = false
+            this.operation1 = false
           })
-          this.isType = false
         } else {
-          this.notifyError("转换错误，请重试")
+          this.$confirm('转换错误，正确答案为：'+ v, '提示',{
+            confirmButtonText: '确定',
+            type:'warning'
+          }).then(()=>{
+            this.dl = this.data.input3
+          })
+        }
+      },
+      //修正前按钮
+      subitfront(){
+        exp10_shuangpinDianLiCeng().then(response => {
+          console.log(response)
+          this.data = response.data
+        })
+        this.wj = null
+        this.dl = null
+        this.frontlist = true
+        this.afterlist = false
+        this.timesbj = false
+        this.timesbj1 = true
+        this.operation = true
+        this.operation1 = false
+      },
+      //修正后按钮
+      subitafter(){
+        exp10_shuangpinDianLiCeng().then(response => {
+          console.log(response)
+          this.data = response.data
+        })
+        this.afternum = null
+        this.aftername = null
+        this.frontlist = false
+        this.afterlist = true
+        this.timesbj = false
+        this.timesbj1 = true
+        this.operation = true
+        this.operation1 = false
+      },
+      //修正后伪距提交按钮
+      submitafter(){
+        var v = this.data.input
+        if (this.afternum  === this.data.input) {
+          this.$confirm('转换正确!', '提示', {
+            type: 'success'
+          }).then(() => {
+            this.timesbj = true
+            this.timesbj1 = false
+            this.operation = false
+            this.operation1 = true
+          })
+          
+        } else {
+          this.$confirm('转换错误，正确答案为：'+ v, '提示',{
+            confirmButtonText: '确定',
+            type:'warning'
+          }).then(()=>{
+            this.afternum = this.data.input
+          })
+        }
+      },
+      //修正后TGD提交按钮
+      submitafter1(){
+         var v = this.data.input2
+        if (this.aftername  === this.data.input2) {
+          this.$confirm('转换正确!', '提示', {
+            type: 'success'
+          }).then(() => {
+            this.timesbj = true
+            this.timesbj1 = true
+            this.operation = false
+            this.operation1 = false
+          })
+          
+        } else {
+          this.$confirm('转换错误，正确答案为：'+ v, '提示',{
+            confirmButtonText: '确定',
+            type:'warning'
+          }).then(()=>{
+            this.aftername = this.data.input2
+          })
         }
       }
-
-    },
   }
 
 }
 </script>
 
 <style scoped>
-
+  .properties{
+    margin-left: 10px;
+  }
+  .clock{
+    margin-bottom: 15px;
+  }
 </style>
--- lazy-ui/src/views/web/vm/GeocenGeostatiRectan.vue
+++ lazy-ui/src/views/web/vm/GeocenGeostatiRectan.vue
@@ -4,56 +4,76 @@
     <div><h2>地心地固直角坐标系与大地坐标系的转换</h2></div>
     <el-divider/>
     <div>
-      <el-card style="position: absolute;left: 5px; top: 42px; width: 40%;height:700px;overflow:auto;">
+      <el-card style="position: absolute;left: 5px; top: 0; width: 40%;height:100%;overflow-y: auto;overflow-x: auto;">
         <div slot="header" class="clearfix">
           <span>实验步骤</span>
           <span style="float: right;color: #498c5f" v-if="data.resource==null">暂未上传实验大纲</span>
           <span v-else style="float: right;color: #498c5f"> <a
             :href="data.resource.url" target="_blank">点击此处下载实验大纲</a></span>
         </div>
-        <div v-html="data.process">
+        <div style="padding-top: 25px" v-html="data.process">
 
         </div>
       </el-card>
     </div>
-    <el-card>
-      <span v-if="isType">
-        地心地固直角坐标系:
-        <el-input v-model="dxdgzj.X" disabled style="width: 170px"/>
-        <el-input v-model="dxdgzj.Y" disabled style="width: 170px"/>
-        <el-input v-model="dxdgzj.Z" disabled style="width: 170px"/>
-        <el-button @click="generateData">刷新</el-button>
-      </span>
-      <span v-else>
-        大地坐标系:
-        <el-input v-model="BLHToXYZ.input.B" style="width: 170px"/>
-        <el-input v-model="BLHToXYZ.input.L" style="width: 170px"/>
-        <el-input v-model="BLHToXYZ.input.H" style="width: 170px"/>
-      </span>
-    </el-card>
-    <el-card class="card" style="margin-top: 10px">
-      <!--      <p>ps:请将地心地固直角坐标系转换成大地坐标系</p>-->
-      <div>
-        <el-form label-width="100px">
-          <el-col :span="24">
-            <el-form-item label="大地坐标系" prop="time">
-              <el-input style="width: 200px"  v-model="ddzbx.B"></el-input>
-              <el-input style="width: 200px"  v-model="ddzbx.L"></el-input>
-              <el-input style="width: 200px"  v-model="ddzbx.H"></el-input>
-              <el-button @click="submitGC">提交</el-button>
-            </el-form-item>
-          </el-col>
-          <el-col :span="24">
-            <el-form-item label="地心地固坐标" prop="time">
-              <el-input style="width: 200px"  v-model="dxdgzbx.X"></el-input>
-              <el-input style="width: 200px"  v-model="dxdgzbx.Y"></el-input>
-              <el-input style="width: 200px"  v-model="dxdgzbx.Z"></el-input>
-              <el-button @click="submitGeocentric">提交</el-button>
-            </el-form-item>
-          </el-col>
-        </el-form>
-      </div>
-    </el-card>
+    <div class="clock" style="margin-bottom:15px;">
+      <el-button @click="subitbd" size="small">地心坐标</el-button>
+      <el-button @click="subitst" size="small">大地坐标</el-button>
+    </div>
+    <div class="allboady" v-show="markes">
+      <el-card>
+        <span>
+          地心地固直角坐标系:
+          <el-input v-model="dxdgzj.X"  style="width: 240px"/>
+          <el-input v-model="dxdgzj.Y"  style="width: 240px"/>
+          <el-input v-model="dxdgzj.Z"  style="width: 240px"/>
+          <el-button @click="generateData">刷新</el-button>
+        </span>
+      </el-card>
+      <el-card class="card" style="margin-top: 10px;font-size: 14px;font-weight: 600;">
+             <p>请将地心地固直角坐标系转换成大地坐标系</p>
+        <div>
+          <el-form label-width="100px">
+            <el-col :span="24">
+              <el-form-item label="大地坐标系：" prop="time">
+                <el-input style="width: 200px"  v-model="ddzbx.B"></el-input>
+                <el-input style="width: 200px"  v-model="ddzbx.L"></el-input>
+                <el-input style="width: 200px"  v-model="ddzbx.H"></el-input>
+                <el-button @click="submitGC">转换</el-button>
+              </el-form-item>
+            </el-col>
+          </el-form>
+        </div>
+      </el-card>
+    </div>
+
+    <div class="allboady" v-show="markenums">
+      <el-card>
+        <span>
+          大地坐标系:
+          <el-input v-model="output.B"  style="width: 240px"/>
+          <el-input v-model="output.L"  style="width: 240px"/>
+          <el-input v-model="output.H"  style="width: 240px"/>
+          <el-button @click="generateData">刷新</el-button>
+        </span>
+      </el-card>
+      <el-card class="card" style="margin-top: 10px;font-size: 14px;font-weight: 600;">
+             <p>请将大地坐标系转换成地心地固直角坐标系</p>
+        <div>
+          <el-form label-width="100px">
+            <el-col :span="24">
+              <el-form-item label="地心地固直角坐标系：" prop="time" label-width="160px">
+                <el-input style="width: 200px"  v-model="geocentric.X"></el-input>
+                <el-input style="width: 200px"  v-model="geocentric.Y"></el-input>
+                <el-input style="width: 200px"  v-model="geocentric.Z"></el-input>
+                <el-button @click="geocentricSubit">提交</el-button>
+              </el-form-item>
+            </el-col>
+          </el-form>
+        </div>
+      </el-card>
+    </div>
+    
   </div>
 </template>
 
@@ -68,10 +88,13 @@ export default {
       isType: true,
       dxdgzj: {},
       output: {},
-      ddzbx: {},
+      ddzbx: {B:'',L:'',H:''},
       BLHToXYZ: {},
-      dxdgzbx: {},
-      data: {}
+      dxdgzbx: {X:'',Y:'',Z:''},
+      data: {},
+      markes:true,
+      markenums:false,
+      geocentric:{X:'',Y:'',Z:''}
     }
   },
   created() {
@@ -124,6 +147,8 @@ export default {
         this.dxdgzj = response.data.input
         this.output = response.data.output
       })
+      this.ddzbx = {B:'',L:'',H:''}
+      this.geocentric = {X:'',Y:'',Z:''}
     },
     submitGC() {
       var system = this.output.B + "," + this.output.L + "," + this.output.H
@@ -139,25 +164,71 @@ export default {
       }else {
         this.$alert('转换错误，正确答案为：\n'+system, '提示', '标题名称', {
           confirmButtonText: '确定',
-          callback: action => {
-
-          }
-        });
-
+          type:'warbing',}).then(()=>{
+            this.ddzbx.B = this.output.B
+            this.ddzbx.L = this.output.L
+            this.ddzbx.H = this.output.H
+          })
       }
     },
     submitGeocentric() {
-      var system = this.BLHToXYZ.output.X + "," + this.BLHToXYZ.output.Y + "," + this.BLHToXYZ.output.Z
-      console.log(system)
-      var user =  this.dxdgzbx.X+ "," +this.dxdgzbx.Y+","+this.dxdgzbx.Z
+      var system = this.dxdgzj.X + "," + this.dxdgzj.Y + "," + this.dxdgzj.Z
+      var user =  this.dxdgzbx.X + "," +this.dxdgzbx.Y + "," + this.dxdgzbx.Z
+      console.log(system,user)
       if (system===user){
         this.notifySuccess("正确", "转换正确")
       }else {
         this.$alert('转换错误，正确答案为：\n'+system, '提示', '标题名称', {
           confirmButtonText: '确定',
-        });
+          type:'warbing',}).then(()=>{
+            this.dxdgzbx.X = this.dxdgzj.X
+            this.dxdgzbx.Y = this.dxdgzj.Y
+            this.dxdgzbx.Z = this.dxdgzj.Z
+          })
       }
-    }
+    },
+
+    //切换地心坐标按钮
+    subitbd(){
+       Experiment().then(response => {
+        console.log(response)
+        this.dxdgzj = response.data.input
+        this.output = response.data.output
+      })
+      this.ddzbx = {B:'',L:'',H:''}
+      this.geocentric = {X:'',Y:'',Z:''}
+      this.markes = true
+      this.markenums = false
+    },
+    //切换大地坐标系按钮
+    subitst(){
+       Experiment().then(response => {
+        console.log(response)
+        this.dxdgzj = response.data.input
+        this.output = response.data.output
+      })
+      this.ddzbx = {B:'',L:'',H:''}
+      this.geocentric = {X:'',Y:'',Z:''}
+      this.markes = false
+      this.markenums = true
+    },
+    //地心地固提交按钮
+    geocentricSubit(){
+      var system = this.geocentric.X + "," + this.geocentric.Y + "," + this.geocentric.Z
+      var user =  this.dxdgzj.X + "," +this.dxdgzj.Y + "," + this.dxdgzj.Z
+      console.log(system,user)
+      if (system===user){
+        this.notifySuccess("正确", "转换正确")
+      }else {
+        this.$alert('转换错误，正确答案为：\n'+user, '提示', '标题名称', {
+          confirmButtonText: '确定',
+          type:'warbing',}).then(()=>{
+            this.geocentric.X = this.dxdgzj.X
+            this.geocentric.Y = this.dxdgzj.Y
+            this.geocentric.Z = this.dxdgzj.Z
+          })
+      }
+    },
   }
 }
 </script>
